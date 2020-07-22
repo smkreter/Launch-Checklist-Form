ff7a318bf3ad5bff653db102258a1b3bc7d76714
unknown <smkreter@gmail.com>
2020-07-22 17:19:38 -0500

    for some reason my or statement when asking if any of the values are blank is failing if the first value isn't blank. I'm bored of this problem and I can dry the code out later so I'm making a bunch of duplicate crap now

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+      form.addEventListener("submit", function(){
+         let pilotInput = document.getElementById("pilotName");
+         let coPilotInput = document.getElementById("copilotName");
+         let fuelInput = document.getElementById("fuelLevel");
+         fuelInput = Number(fuelInput);
+         let massInput = document.getElementById("cargoMass");
+         massInput = Number(massInput);
+         /*
+         console.log("pilot name: " pilotInput.value);
+         console.log("copilot name: " + coPilotInput.value);
+         console.log("fuel input amount: " + fuelInput.value);
+         console.log("mass input amount: " + massInput.value);
+         */
+            if ((pilotInput.value === "") || (coPilotInput.value === "") || (fuelInput.value === "") || (massInput.value === "")) {
+               alert("All fields are required");
+               console.log("pilot name: " + pilotInput.value);
+               /*
+               console.log("copilot name: " + coPilotInput.value);
+               console.log("fuel input amount: " + fuelInput.value);
+               console.log("mass input amount: " + massInput.value);
+               */
+               event.preventDefault();
+            }else{
+               if ((pilotInput.value != "") && (isNaN(pilotInput.value) === false)) {
+               alert("Input a string for the name of the pilot")
+               event.preventDefault();
+                   }
+               if ((coPilotInput.value != "") && (isNaN(coPilotInput.value) === false)) {
+               alert("Input a string for the name of the copilot")
+               event.preventDefault();
+                   }
+            if (isNaN(fuelInput.value) === true) {
+               alert("Input a valid number for the fuel level")
+               event.preventDefault();
+                   }
+            if (isNaN(massInput.value) === true) {
+               alert("Input a valid number for the fuel level")
+               event.preventDefault();
+                  }
+             
+            }
+            
 
+         });
+
+
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
