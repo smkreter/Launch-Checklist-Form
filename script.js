8810e29c51c826605cc70deadc122e151c42ca36
unknown <smkreter@gmail.com>
2020-07-22 18:14:24 -0500

    okay the problem was that I'd mislabeled the second item in the list. so having fixed that all is good. now I'm working on the shuttle requirements. does the first one work. we're just seeing if pilot status changes the innerhtml

--- script.js
+++ script.js
@@ -5,43 +5,56 @@ window.addEventListener("load", function(){
          let pilotInput = document.getElementById("pilotName");
          let coPilotInput = document.getElementById("copilotName");
          let fuelInput = document.getElementById("fuelLevel");
-         fuelInput = Number(fuelInput);
+         //fuelInput.value = Number(fuelInput.value);
          let massInput = document.getElementById("cargoMass");
-         massInput = Number(massInput);
+         //massInput.value = Number(massInput.value);
          /*
          console.log("pilot name: " pilotInput.value);
          console.log("copilot name: " + coPilotInput.value);
          console.log("fuel input amount: " + fuelInput.value);
          console.log("mass input amount: " + massInput.value);
+         (coPilotInput.value === "") || (fuelInput.value === "") || (massInput.value === "")
          */
-            if ((pilotInput.value === "") || (coPilotInput.value === "") || (fuelInput.value === "") || (massInput.value === "")) {
+        // validation town
+            if (pilotInput.value === "" || coPilotInput.value === "" || fuelInput.value === "" || massInput.value === ""){
                alert("All fields are required");
-               console.log("pilot name: " + pilotInput.value);
-               /*
-               console.log("copilot name: " + coPilotInput.value);
-               console.log("fuel input amount: " + fuelInput.value);
-               console.log("mass input amount: " + massInput.value);
-               */
                event.preventDefault();
-            }else{
-               if ((pilotInput.value != "") && (isNaN(pilotInput.value) === false)) {
-               alert("Input a string for the name of the pilot")
-               event.preventDefault();
-                   }
-               if ((coPilotInput.value != "") && (isNaN(coPilotInput.value) === false)) {
-               alert("Input a string for the name of the copilot")
+               } else {
+
+               massInput.value = Number(massInput.value);
+               fuelInput.value = Number(fuelInput.value);
+               
+
+               if (isNaN(pilotInput.value) === false) {
+               alert("Input a string for the name of the pilot, Elon")
                event.preventDefault();
                    }
-            if (isNaN(fuelInput.value) === true) {
+
+               if (isNaN(coPilotInput.value) === false) {
+               alert("Input a string for the name of the copilot, Elon")
+               event.preventDefault();
+                   }
+
+               if (isNaN(fuelInput.value) === true) {
                alert("Input a valid number for the fuel level")
                event.preventDefault();
                    }
-            if (isNaN(massInput.value) === true) {
-               alert("Input a valid number for the fuel level")
+
+               if (isNaN(massInput.value) === true) {
+               alert("Input a valid number for the cargo mass")
                event.preventDefault();
-                  }
+                  } 
+
+               } else {
+                  let testLaunchReadiness = true;
+               }
+         // shuttle requirements
+               if (testLaunchReadiness === true) {
+                  let pilotStatus = document.getElementById("pilotStatus");
+                  pilotStatus.innerHTML = "Pilot Name: "+ pilotInput.value;
+               }
              
-            }
+            
             
 
          });
