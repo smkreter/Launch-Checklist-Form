9df84fb975176efd661f457c6ebf56ca335f4e8c
unknown <smkreter@gmail.com>
2020-07-25 18:36:22 -0500

    updated the messages to make them reasonable. deleted a bunch of old trash code that I'd tried and hadn't worked

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ window.addEventListener("load", function(){
       return response.json();
       }).then(function(json){
          let missionTarget = document.getElementById("missionTarget");
-         console.log(json);
          let planet = json[0];
-         console.log(planet);
-         console.log(planet.name);
-        
+
+   // Add the information from json into the HTML of the page     
           
          missionTarget.innerHTML += `
             <h2>Mission Destination</h2>
@@ -30,14 +28,7 @@ window.addEventListener("load", function(){
    });
 
 
-      form.addEventListener("submit", function(){
-         /*
-         console.log(`For pilotInput, the value is ${pilotInput.value} and the type of the value is ${typeof pilotInput.value}.`);
-         console.log(`For coPilotInput, the value is ${coPilotInput.value} and the type of the value is ${typeof coPilotInput.value}.`);
-         console.log(`For massInput, the value is ${massInput.value} and the type of the value is ${typeof massInput.value}.`);
-         console.log(`For fuelInput, the value is ${fuelInput.value} and the type of the value is ${typeof fuelInput.value}.`);
-             */
-
+   form.addEventListener("submit", function(){
 
    // Variables - Inputs
 
@@ -60,20 +51,20 @@ window.addEventListener("load", function(){
 
    let pilotNameTest = {
       "test": (isNaN(pilotInput.value)),
-      "error": "Input a string for the name of the pilot "
+      "error": " Input a string for the name of the pilot "
    };
 
    let coPilotNameTest = {
       "test": (isNaN(coPilotInput.value)),
-      "error": "Input a string for the name of the copilot "
+      "error": " Input a string for the name of the copilot "
    };
    let fuelNumberTest = {
       "test": (!isNaN(fuelInput.value)),
-      "error": "Input a valid number for the fuel level "
+      "error": " Input a valid number for the fuel level "
    };
    let massNumberTest = {
       "test": (!isNaN(massInput.value)),
-      "error": "Input a valid number for the cargo mass "
+      "error": " Input a valid number for the cargo mass "
    };
 
    let testArray = [pilotNameTest, coPilotNameTest, fuelNumberTest, massNumberTest];
@@ -81,27 +72,25 @@ window.addEventListener("load", function(){
    let testedTests = [];
 
         // Validation
+
          //All fields are required
        
             if (pilotInput.value === "" || coPilotInput.value === "" || fuelInput.value === "" || massInput.value === ""){
                alert("All fields are required");
                event.preventDefault();
                }else {
+
          //The right value types are required
 
                // pilot name should not be a number
                let pilotInputTesterVariable = (Number(pilotInput.value));
-               //console.log(pilotInputTesterVariable);
                if (isNaN(Number(pilotInputTesterVariable)) === false) {
-               //  console.log(`pilotInputTesterVariable is ${pilotInputTesterVariable}`)
                   pilotInput.value = pilotInputTesterVariable;
                }
 
                // copilot name should not be a number
                let coPilotInputTesterVariable = (Number(coPilotInput.value));
-               // console.log(coPilotInputTesterVariable);
                if (isNaN(Number(coPilotInputTesterVariable)) === false) {
-               //  console.log(`coPilotInputTesterVariable is ${coPilotInputTesterVariable}`)
                   coPilotInput.value = coPilotInputTesterVariable;
                }
 
@@ -111,8 +100,7 @@ window.addEventListener("load", function(){
                // fuel input should be a number
                fuelInput.value = Number(fuelInput.value);
                
-               //console.log(testArray.length);
-
+               // run these tests
                for (i = 0; i < testArray.length; i++) {
                   testedTests.push(testArray[i]);
                   if (testArray[i].test === false) {
@@ -127,65 +115,44 @@ window.addEventListener("load", function(){
                   readyToTest = true;
                   event.preventDefault();
                }
-               // Printing out the values to see that we get what's expected
-               /*
-               console.log(`For pilotInput, the value is ${pilotInput.value} and the type of the value is ${typeof pilotInput.value}.`);
-               console.log(`For coPilotInput, the value is ${coPilotInput.value} and the type of the value is ${typeof coPilotInput.value}.`);
-               console.log(`For massInput, the value is ${massInput.value} and the type of the value is ${typeof massInput.value}.`);
-               console.log(`For fuelInput, the value is ${fuelInput.value} and the type of the value is ${typeof fuelInput.value}.`);
-               console.log("here's testArray:");
-               console.log(testArray);
-               console.log("here's errorArray:");
-               console.log(errorArray);
-               console.log(`readytotest is ${readyToTest}`);
-               // console.log("here's testedTests:");
-               // console.log(testedTests);
-               */
-
    
                // faulty input styling and content
+
                   if (readyToTest === true) {
                         faultyItems.style.visibility = "visible";
                         pilotStatus.innerHTML = `Pilot ${pilotInput.value} is ready for launch`;
                         copilotStatus.innerHTML = `Co-Pilot ${coPilotInput.value} is ready for launch`;  
+
                            // change stuff based on the inputs
                            if ( (massInput.value > 10000) || (fuelInput.value < 10000) ) {
                               launchStatus.style.color = "red";
-                              launchStatus.innerHTML = "Don't Launch Yet Jeez"    
+                              launchStatus.innerHTML = "Shuttle not ready for launch"    
                               if (massInput.value > 10000) {
                                  cargoStatus.innerHTML = "Too much stuff to launch, put stuff back";                                       
                               }   
                               if (fuelInput.value < 10000) {
-                                    fuelStatus.innerHTML = "Fuel level not high enough for launch, get more fuel";
+                                    fuelStatus.innerHTML = "Fuel level too low for launch";
                                  }
                           
                            } else {
                               launchStatus.style.color = "green";
-                              launchStatus.innerHTML = "All right launch away"
+                              launchStatus.innerHTML = "Shuttle is ready for launch";
+                              fuelStatus.innerHTML = "Fuel level high enough for launch";
+                              cargoStatus.innerHTML = "Cargo mass low enough for launch";
                            }
 
                                        
 // if ready to test is true
-                               }                                       
-                           
-
-
-
-
-
+                      }                                       
 
 // else if all of the inputs are inputted
                }
-               
-
-
 
 // the submission event listener
-               });
-
+      });
       
 // the onload event listener
-               }); 
+   }); 
 
 
 
